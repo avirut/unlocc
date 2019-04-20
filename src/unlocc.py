e95e499ff0e74573fb9f3f5d441636f45446660c
avirut <avirut.mehta@gmail.com>
2019-04-19 21:57:50 -0500

    vaguely working caps lock toggle

--- src/unlocc.py
+++ src/unlocc.py
@@ -1,7 +1,28 @@
 import keyboard
+from threading import Timer
 
-recorded = keyboard.record(until='escape')
 
-for keyPress in recorded:
-    if keyPress.event_type == keyboard.KEY_DOWN:
-        print(keyPress.name)
+masterTimer = None
+
+
+def resetTimer():
+    global masterTimer
+    masterTimer.cancel()
+    masterTimer = None
+    print('reset the timer')
+
+
+def handleMaster(key):
+    global masterTimer
+    if masterTimer is not None:
+        print(masterTimer)
+        keyboard.send(key.name)
+    masterTimer = Timer(0.3, resetTimer)
+    masterTimer.start()
+
+
+keyboard.on_press_key(key='caps lock', callback=handleMaster, suppress=True)
+
+# keyboard.on_press(callback=lambda key: print('pressed ' + key.name))
+
+input('hello')
